d387858cd735ef3f0646d0cfc45f8f537ec532a8
Kenny200 <58740689+Kenny200@users.noreply.github.com>
2023-11-19 10:51:24 -0500

    Update BalloutDemo.java

--- BalloutDemo.java
+++ BalloutDemo.java
@@ -8,22 +8,21 @@ public class BalloutDemo {
     private List<Ballot> ballots = new ArrayList<>();
 
     public BalloutDemo() {
-        ballots.add(new Ballot("Alice", Arrays.asList("Alice", "Bob", "Charlie")));
-        ballots.add(new Ballot("Bob", Arrays.asList("Bob", "Charlie", "Alice")));
-        ballots.add(new Ballot("Charlie", Arrays.asList("Charlie", "Alice", "Bob")));
+        ballots.add(new Ballot("Alice", Arrays.asList("Alice", "Bob", "Charlie", "John", "Jane")));
+        ballots.add(new Ballot("Bob", Arrays.asList("Bob", "Charlie", "Alice", "John", "Jane")));
+        ballots.add(new Ballot("Charlie", Arrays.asList("Charlie", "Alice", "Bob", "John", "Jane")));
     }
 
     public String simulateRunoffElection() {
-    //Print the Ballots and Results
+        // Print the number of voters and ranks for each ballot
+        printRoundInfo();
+
+        /* Print the ballots and results before sorting and eliminating(for testing purposes)
         if (ballots.size() > 1) {
-        System.out.println("Round results:");
-        for (Ballot ballot : ballots) {
-            System.out.println(ballot.getCandidate() + ": " + ballot.getPreferences());
+            printRoundResults(ballots);
         }
-        System.out.println();
-        
-        return simulateRunoffElection();
-    }
+        */
+
         ballots.sort((b1, b2) -> Integer.compare(b2.getPreferences().indexOf(b2.getCandidate()), b1.getPreferences().indexOf(b1.getCandidate())));
 
         String eliminatedCandidate = ballots.get(ballots.size() - 1).getCandidate();
@@ -39,12 +38,28 @@ public class BalloutDemo {
             return simulateRunoffElection();
         }
 
-        return ballots.get(0).getCandidate();
+        String winner = ballots.get(0).getCandidate();
+        System.out.println("Winner: " + winner);
+        return winner + " is the winner";
     }
+
+    private void printRoundInfo() {
+        System.out.println("Number of voters: " + ballots.size());
+
+        for (Ballot ballot : ballots) {
+            System.out.println("Rank 1: " + ballot.getPreferences().get(0));
+            System.out.println("Rank 2: " + ballot.getPreferences().get(1));
+            System.out.println("Rank 3: " + ballot.getPreferences().get(2));
+            System.out.println();
+        }
+    }
+
+    
+
 
     public static void main(String[] args) {
     	BalloutDemo demo = new BalloutDemo();
         String winner = demo.simulateRunoffElection();
-        System.out.println("The winner of the runoff election is: " + winner);
+        //System.out.println(winner + "is the winner");
     }
 }
