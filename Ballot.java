7b89b044926f65a180c589f65d1f039633d23754
Kenny200 <58740689+Kenny200@users.noreply.github.com>
2023-11-19 08:54:21 -0500

    Update Ballot.java

--- Ballot.java
+++ Ballot.java
@@ -1,17 +1,16 @@
 //Kenneth Ortega November 17,2023
 import java.util.List;
-
+//Ballot class file
 public class Ballot {
     private String candidate;
     private List<String> preferences;
-
+//Ballot object which uses our string candidate and our List String preferences
     public Ballot(String candidate, List<String> preferences) {
         this.candidate = candidate;
         this.preferences = preferences;
     }
-    
-    public void setCandidate(String string) {
-		
+    //the getters and setters
+    public void setCandidate(String string) {	
 		
 	}
     
